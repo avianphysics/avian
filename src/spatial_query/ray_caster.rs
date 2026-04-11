e6110860cc536df745cfc4260cca942baaf2f018
Hennadii Chernyshchyk <genaloner@gmail.com>
2026-04-12 01:56:51 +0300

    Deref the direction

--- src/spatial_query/ray_caster.rs
+++ src/spatial_query/ray_caster.rs
@@ -283,13 +283,13 @@ impl RayCaster {
     /// Returns the point at a given distance along the ray.
     #[must_use]
     pub fn get_point(&self, distance: f32) -> Vector {
-        self.origin + self.direction * distance
+        self.origin + *self.direction * distance
     }
 
     /// Like [`Self::get_point`], but returns the point in global coordinates.
     #[must_use]
     pub fn get_global_point(&self, distance: f32) -> Vector {
-        self.global_origin + self.global_direction * distance
+        self.global_origin + *self.global_direction * distance
     }
 }
 
