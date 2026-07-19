721f6b439ce8f5a9904a03d5d530b3b004f73eaf
Joona Aalto <jondolf.dev@gmail.com>
2026-07-19 16:48:42 +0300

    Fix normals in character controller examples

--- crates/avian3d/examples/kinematic_character_3d/plugin.rs
+++ crates/avian3d/examples/kinematic_character_3d/plugin.rs
@@ -190,7 +190,7 @@ fn update_grounded(
         // The character is grounded if we hit a surface that isn't too steep
         let is_grounded = hit.is_some_and(|hit| {
             let up = global_transform.up();
-            (rotation * hit.normal1).angle_between(*up) <= ground_detection.max_angle
+            (rotation * hit.normal).angle_between(*up) <= ground_detection.max_angle
         });
 
         // Update grounded state
